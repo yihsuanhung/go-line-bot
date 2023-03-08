a48dd45c4d4a4424156dd98446ca86756e9a1823
yihsuanhung <yihsuan.simon@gmail.com>
2023-03-08 20:50:40 +0800

    chore: config minor change

--- cmd/line_bot/main.go
+++ cmd/line_bot/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	cmd.Execute()
 	cfg := conf.NewConfig(
-		conf.WithPath("./chat/app"), // ./chat/app ../../chat/app
+		conf.WithPath("../../chat/app"), // ./chat/app ../../chat/app
 		conf.WithType("yaml"),
 	)
 	var botInfo settings.LineBotInfo
